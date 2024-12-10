38865d0006631517c02338447ceb949e37621bb8
Ankush Kumar Kushwaha <ankush@radiantinfonet.com>
2024-12-10 14:44:17 +0530

    check

--- src/index.js
+++ src/index.js
@@ -14,7 +14,7 @@ connectDB()
         throw error;
     })
     app.listen(process.env.PORT||8000,()=>{
-        console.log(`Server is running chill karo PORT : ${process.env.PORT}`);
+        console.log(`Server is running chill karo PORT : ${process.env.PORT}`)
     })
 })
 .catch((error)=>{
